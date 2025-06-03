4b078ca3beb775d38e1de79fae9fdce2b5ef86ca
abdurahmon086 <1937ali047@gmail.com>
2025-06-03 11:06:06 +0500

    feat: update CORS configuration to allow all origins

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   // CORS sozlamalari
   app.enableCors({
-    origin: 'https://batareykalar.uz',
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
